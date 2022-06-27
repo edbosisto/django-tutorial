8bb9cd0a69261c16bc2b614394149b340746cdd1
edbosisto <e.bosisto@gmail.com>
2022-06-27 11:06:08 +0200

    for and if statement examples within block content for ToDoList

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -5,10 +5,9 @@ from .models import ToDoList, Item
 # Create your views here.
 
 
-def index(response, name):
-    ls = ToDoList.objects.get(name=name)
-    item = ls.item_set.get(id=1)
-    return render(response, "main/base.html", {})
+def index(response, id):
+    ls = ToDoList.objects.get(id=id)
+    return render(response, "main/list.html", {"ls": ls})
 
 
 def home(response):
