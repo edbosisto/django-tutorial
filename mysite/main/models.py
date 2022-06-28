d1f7798dc7c9581388ad51ceb81a654aceae3bf9
edbosisto <e.bosisto@gmail.com>
2022-06-28 11:37:14 +0200

    added user to ToDoList model and migrated models

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here. After creating or updating models use 2 lines below in cmd to migrate the classes.
 # python manage.py makemigrations main
@@ -6,6 +7,7 @@ from django.db import models
 
 
 class ToDoList(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
 
     def __str__(self):
